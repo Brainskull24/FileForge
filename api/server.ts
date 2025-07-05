7b0b7ba4786449855a918d795a86eb8f4f7b6400
Brainskull <garg.nimit36@gmail.com>
2025-07-05 13:13:20 +0530

    Layout fixes

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,19 @@
 import dotenv from 'dotenv';
-dotenv.config();
-import app from './app';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import authRoutes from './routes/authRoutes';
 import { connectDB } from './config/db';
+
+dotenv.config();
+const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(express.json())
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use('/api/v1/auth', authRoutes);
 
 const PORT = process.env.PORT || 4000;
 
