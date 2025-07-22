495379cb09040f8058c3f3aee80780ea6e9e043f
Brainskull <garg.nimit36@gmail.com>
2025-07-22 17:20:41 +0530

    delete worker

--- worker/app/schemas/convert_schema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class ConvertResponse(BaseModel):
-    success: bool
-    content: str
--- worker/app/utils/file_helpers.py
+++
@@ -1,4 +0,0 @@
-from fastapi import UploadFile
-
-async def read_file_content(file: UploadFile) -> bytes:
-    return await file.read()
